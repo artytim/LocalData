f7c7f29b3aa375ab5cbd2b4a4000f5ae7e61062c
artytim <arty.tim@gmail.com>
2017-03-14 11:50:08 +0800

    Initial commit - Java data model & collections

---
+++ app/src/main/java/com/example/android/data/model/DataItem.java
@@ -0,0 +1,98 @@
+package com.example.android.data.model;
+
+/**
+ * Created by timshum on 3/14/2017.
+ */
+
+public class DataItem {
+    // Better using only Java primitive data types & String - easy to pass data objects between tiers of app
+    private String itemId; // In Android, it's common practice to use string value as the Primary key (UUID)
+    private String itemName;
+    private String description;
+    private String category; // might also be a UUID
+    private int sortPosition;
+    private double price;
+    private String image;
+
+    public DataItem() {
+    }
+
+    public DataItem(String itemId, String itemName, String description, String category, int sortPosition, double price, String image) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.description = description;
+        this.category = category;
+        this.sortPosition = sortPosition;
+        this.price = price;
+        this.image = image;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getSortPosition() {
+        return sortPosition;
+    }
+
+    public void setSortPosition(int sortPosition) {
+        this.sortPosition = sortPosition;
+    }
+
+    @Override
+    public String toString() {
+        return "DataItem{" +
+                "itemId='" + itemId + '\'' +
+                ", itemName='" + itemName + '\'' +
+                ", description='" + description + '\'' +
+                ", category='" + category + '\'' +
+                ", sortPosition=" + sortPosition +
+                ", price=" + price +
+                ", image='" + image + '\'' +
+                '}';
+    }
+}

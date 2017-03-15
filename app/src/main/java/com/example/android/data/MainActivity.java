144941d729c12d1bf6574ef09c84f7e3040b1e03
artytim <arty.tim@gmail.com>
2017-03-15 15:48:47 +0800

    Manage shared preferences with Java

--- app/src/main/java/com/example/android/data/MainActivity.java
+++ app/src/main/java/com/example/android/data/MainActivity.java
@@ -1,24 +1,26 @@
 package com.example.android.data;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.android.data.model.DataItem;
 import com.example.android.data.sample.SampleDataProvider;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    @SuppressWarnings("FieldCanBeLocal")
-    private TextView tvOut;
+    private static final int SIGNIN_REQUEST = 1001;
+    public static final String MY_GLOBAL_PREFS = "my_global_prefs";
     List<DataItem> dataItemList = SampleDataProvider.dataItemList;
-    List<String> itemNames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +34,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        for (DataItem item: dataItemList) {
-//            itemNames.add(item.getItemName());
-//        }
-//        Collections.sort(itemNames);
-
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String> (
-//                this, android.R.layout.simple_list_item_1, itemNames);
-
         DataItemAdapter adapter = new DataItemAdapter(this, dataItemList);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rvItems);
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_signin:
+                Intent intent = new Intent(this, SigninActivity.class);
+                startActivityForResult(intent, SIGNIN_REQUEST);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == SIGNIN_REQUEST) {
+            String email = data.getStringExtra(SigninActivity.EMAIL_KEY);
+            Toast.makeText(this, "You signed in as " + email, Toast.LENGTH_SHORT).show();
+
+            SharedPreferences.Editor editor =
+                    getSharedPreferences(MY_GLOBAL_PREFS, MODE_PRIVATE).edit();
+            editor.putString(SigninActivity.EMAIL_KEY, email);
+            editor.apply();
+        }
     }
 }

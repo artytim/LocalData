8e5835ea9b9a9a1f45d042b4f61f952480775c44
artytim <arty.tim@gmail.com>
2017-03-15 11:44:18 +0800

    Pass intent extras to detail activities

--- app/src/main/java/com/example/android/data/DataItemAdapter.java
+++ app/src/main/java/com/example/android/data/DataItemAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.data;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -18,6 +19,7 @@ import java.util.List;
 
 public class DataItemAdapter extends RecyclerView.Adapter<DataItemAdapter.ViewHolder> {
 
+    public static final String ITEM_ID_KEY = "item_id_key";
     private List<DataItem> mItems;
     private Context mContext;
 
@@ -52,8 +54,12 @@ public class DataItemAdapter extends RecyclerView.Adapter<DataItemAdapter.ViewHo
 
             @Override
             public void onClick(View v) {
-                Toast.makeText(mContext, "You selected " + item.getItemName(),
-                        Toast.LENGTH_SHORT).show();
+//                Toast.makeText(mContext, "You selected " + item.getItemName(),
+//                        Toast.LENGTH_SHORT).show();
+                String itemId = item.getItemId();
+                Intent intent = new Intent(mContext, DetailActivity.class);
+                intent.putExtra(ITEM_ID_KEY, itemId);
+                mContext.startActivity(intent); // launch the activity
             }
         });
 
--- app/src/main/java/com/example/android/data/model/DataItem.java
+++ app/src/main/java/com/example/android/data/model/DataItem.java
@@ -1,5 +1,7 @@
 package com.example.android.data.model;
 
+import java.util.UUID;
+
 /**
  * Created by timshum on 3/14/2017.
  */
@@ -18,6 +20,11 @@ public class DataItem {
     }
 
     public DataItem(String itemId, String itemName, String description, String category, int sortPosition, double price, String image) {
+
+        if (itemId == null) {
+            itemId = UUID.randomUUID().toString();
+        }
+
         this.itemId = itemId;
         this.itemName = itemName;
         this.description = description;
